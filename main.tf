f771187e0d6c7f6acbd564893ed104a6b5c3874e
NacreousNet <nacreous@cybersecurity.com>
2022-11-27 12:11:04 +0000

    update

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ resource "google_project" "project" {
 # Use `gcloud` to enable:
 # - serviceusage.googleapis.com
 # - cloudresourcemanager.googleapis.com
-resource "null_resource" "enable_service_usage_api" {
-  provisioner "local-exec" {
-    command = "gcloud services enable  compute.googleapis.com --project ${var.gcp_project_id}"
-  }
+#resource "null_resource" "enable_service_usage_api" {
+ # provisioner "local-exec" {
+    #command = "gcloud services enable  compute.googleapis.com --project ${var.gcp_project_id}"
+  #}
 
 #serviceusage.googleapis.com cloudresourcemanager.googleapis.com
 
@@ -28,4 +28,4 @@ resource "null_resource" "enable_service_usage_api" {
   ]
 
   */
-}
+#}
